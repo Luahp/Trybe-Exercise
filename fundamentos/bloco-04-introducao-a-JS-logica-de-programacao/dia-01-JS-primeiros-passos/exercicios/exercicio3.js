abf8aba431a7f31484e75bc205f47b8eb527762e
Luahp <luahporto@gmail.com>
2022-07-21 19:30:07 -0300

    exercicios de 1 a 5

---
+++ fundamentos/bloco-04-introducao-a-JS-logica-de-programacao/dia-01-JS-primeiros-passos/exercicios/exercicio3.js
@@ -0,0 +1,11 @@
+const a = 2;
+const b = 3;
+const c = 4;
+
+if (a > b && a > c) {
+  console.log("'a' é o maior número!");
+} else if (b > a && b > c) {
+  console.log("'b' é o maior número!");
+} else if (c > a && c > b) {
+  console.log("'c' é o maior número!");
+}
---
+++ fundamentos/bloco-04-introducao-a-JS-logica-de-programacao/dia-01-JS-primeiros-passos/exercicios/exercicios4.js
@@ -0,0 +1,12 @@
+const number = 0;
+
+let resultado;
+
+if (number > 0) {
+  resultado = 'positive';
+} else if (number < 0) {
+  resultado = 'negative';
+} else {
+  resultado = 'zero';
+}
+console.log(resultado);
