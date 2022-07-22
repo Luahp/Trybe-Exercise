ef9bd1f86128fcba57123f41a961b916b13bcda1
Luahp <luahporto@gmail.com>
2022-07-21 22:20:01 -0300

    Terminei o exercicio

--- fundamentos/bloco-04-introducao-a-JS-logica-de-programacao/dia-01-JS-primeiros-passos/exercicios/exercicio10.js
+++ fundamentos/bloco-04-introducao-a-JS-logica-de-programacao/dia-01-JS-primeiros-passos/exercicios/exercicio10.js
@@ -4,6 +4,7 @@ const saleValue = 4;
 if (costOfProduct >= 0 && saleValue >= 0) {
   const totalCostOfProduct = costOfProduct * 1.2;
   const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+
   console.log(totalProfit);
 } else {
   console.log('Error');
