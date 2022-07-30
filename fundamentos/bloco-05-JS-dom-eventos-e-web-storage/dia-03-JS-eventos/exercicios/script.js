266f4837ae22c40cc99f8eab2f78f94e5e561858
Luahp <luahporto@gmail.com>
2022-07-30 00:02:07 -0300

    inicializando exercicios

---
+++ fundamentos/bloco-05-JS-dom-eventos-e-web-storage/dia-03-JS-eventos/exercicios/script.js
@@ -0,0 +1,66 @@
+function createDaysOfTheWeek() {
+  const weekDays = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ];
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+let decemberDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+
+function createDaysOfTheMonth() {
+  let getDaysList = document.querySelector('#days');
+
+  for (let contador = 0; contador < decemberDaysList.length; contador++) {
+    let day = decemberDaysList[contador];
+    let dayIten = document.createElement('li');
+    dayIten.innerText = day;
+
+    if (day === 24 || day === 31) {
+      dayIten.className = 'day holiday';
+      getDaysList.appendChild(dayIten);
+    } else if (day === 4 || day === 11 || day === 19) {
+      dayIten.className = 'day friday';
+      getDaysList.appendChild(dayIten);
+    } else if (day === 25) {
+      dayIten.className = 'day holiday friday';
+      getDaysList.appendChild(dayIten);
+    } else {
+      dayIten.className = 'day';
+      getDaysList.appendChild(dayIten);
+    }
+  }
+}
+createDaysOfTheMonth();
+
+function createHolildayButton(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container');
+  let newButton = ddocument.createElement('button');
+  let newButtonID = 'btn-holiday';
+
+  newButton.innerHTML = buttonName;
+  newButton.id = newButtonID;
+
+  buttonContainer.appendChild(newButton);
+}
+createHolildayButton('Feriados');
